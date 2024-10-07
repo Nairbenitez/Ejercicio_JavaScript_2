17f8cee72aab40961041d8ff4f6a0602e2c3989f
Nairbenitez <beniteznair914@gmail.com>
2024-10-07 16:59:39 -0300

    Corrección.

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -88,18 +88,25 @@ function obtenerDiaSemana(dia) {
     switch (dia) {
         case 1:
         console.log("lunes");
+        break;
         case 2: 
         console.log("martes");
+        break;
         case 3: 
         console.log("miercoles");
+        break;
         case 4: 
         console.log("jueves");
+        break;
         case 5:
         console.log("viernes");
+        break;
         case 6:
         console.log("sabado");
+        break;
         case 7:
         console.log("domingo");
+        break;
         default:
         console.log("ingresa un numero del 1 al 7.");
 
@@ -117,6 +124,7 @@ function esVocal(letra) {
         case "o":
         case "u":
         console.log(true)
+        break;
 
     }
 }
